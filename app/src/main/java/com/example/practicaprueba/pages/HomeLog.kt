5a313e05ea22f5692433ce3bf84adda6fb319e52
Jose Astorga <joseastorgavaras@gmail.com>
2025-11-05 01:14:05 -0300

    hot-fix HomeLog

--- app/src/main/java/com/example/practicaprueba/pages/HomeLog.kt
+++ app/src/main/java/com/example/practicaprueba/pages/HomeLog.kt
@@ -1,12 +1,18 @@
 package com.example.practicaprueba.pages
 
-
+import android.icu.number.Scale
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.practicaprueba.R
 import com.example.practicaprueba.components.CajaLog
 import com.example.practicaprueba.components.imgLogo
 
@@ -20,8 +26,8 @@ fun HomeLog(navController: NavController) {
         verticalArrangement = Arrangement.Center
     ) {
         imgLogo()
-
-        Image(
+        CajaLog(navController = navController)
+        /*Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "Logo de fondo",
             modifier = Modifier
@@ -52,6 +58,6 @@ fun HomeLog(navController: NavController) {
 
                 CajaLog(navController)
             }
-        }
+        }*/
     }
 }
