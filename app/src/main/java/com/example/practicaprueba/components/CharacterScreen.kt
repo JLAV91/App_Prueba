002dfb49b01a89a81cf6387f1574682a61c3f06f
Jose Astorga <joseastorgavaras@gmail.com>
2025-11-04 22:46:19 -0300

    commit despues de stash

---
+++ app/src/main/java/com/example/practicaprueba/components/CharacterScreen.kt
@@ -0,0 +1,53 @@
+package com.example.practicaprueba.components
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun CharacterScreen(modifier: Modifier = Modifier, viewModel: CharacterViewModel = viewModel ()) {
+    val characters = viewModel.characters.value // Accedo al valor de State para obtener la lista de personajes
+    val errorMessage = viewModel.errorMessage.value // Accedo al valor de State para obtener el mensaje de error
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchCharacters()
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        // Verifica si hay un mensaje de error y lo muestra en la pantalla
+        if (errorMessage != null) {
+            Text(text = errorMessage) // Muestra el mensaje de error si existe
+        } else {
+            // Muestra la lista de personajes en un LazyColumn
+            LazyColumn {
+                items(characters) { character ->
+                    val painter = rememberAsyncImagePainter(model = character.image)
+
+                    CardConImagen2(
+                        imagen = painter,
+                        titulo = character.name,
+                        descripcion = "Especie: ${character.species} | Género: ${character.gender}",
+                        ultimoTexto = "Origen: ${character.originName}",
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(modifier = Modifier.height(12.dp))
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/practicaprueba/pages/HomeLog.kt
@@ -0,0 +1,32 @@
+package com.example.practicaprueba.pages
+
+import android.icu.number.Scale
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.practicaprueba.R
+import com.example.practicaprueba.components.CajaLog
+import com.example.practicaprueba.components.imgLogo
+
+@Composable
+fun HomeLog(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        imgLogo()
+
+        CajaLog(navController = navController)
+    }
+}
